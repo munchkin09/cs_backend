04f00be4fa70160019957883f738a65d332a1cf3
CarlosL <carlos.ledesma1983@gmail.com>
2025-06-11 19:56:03 +0200

    feat: reorganizar importaciones y agregar rutas para Swagger UI y documentación

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import helmet from 'helmet';
+import path from 'path';
+import { cwd } from 'process';
 import express from 'express';
+import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import type { Request, Response } from 'express';
 import { IConfiguration } from './types';
 import database from './controllers/db/database';
+import SwaggerUIDist from 'swagger-ui-dist';
 import { buildUserRouter , buildGenerationRouter, buildAuthRouter } from './routes/v1/';
+
+
 
 // Importa los routers de cada versión y responsabilidad
 // Puedes seguir importando más routers según crezcas
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const configuration: IConfiguration = {
@@ -23,8 +28,22 @@ app.use(helmet());
 
 app.use(bodyParser.json());
 
-const {router: authRouter, authMiddleware } = buildAuthRouter(app);
+const swaggerUiAssetPath = SwaggerUIDist.getAbsoluteFSPath();
+const { router: authRouter, authMiddleware } = buildAuthRouter(app);
+
+app.use('/docs', express.static(swaggerUiAssetPath));
+
+app.get('/swagger.json', (req: Request, res: Response) => {
+    res.sendFile(path.join(cwd(), 'swagger', 'openapi.json'));
+});
 app.use(authMiddleware);
+
+// Endpoint raíz
+app.use(express.static(path.join(cwd(), 'static')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(cwd(), 'static', 'index.html'));
+});
 
 // Monta los routers por versión y responsabilidad
 app.use('/auth', authRouter);
@@ -37,15 +56,10 @@ app.use((req, res, next) => {
     res.status(404).json({ error: 'Not Found' });
 })
 
-// Endpoint raíz
-app.get('/', (_req, res) => {
-    res.json({ message: 'API running' });
-});
-
 app.listen(PORT, async () => {
+    await database.connect(configuration.databaseUrl, configuration.databaseName);
     console.log(`Server running on port ${PORT}`);
     console.log(`LLM Provider: ${configuration.llmProvider}`);
     console.log(`Database URL: ${configuration.databaseUrl}`);
     console.log(`Database Name: ${configuration.databaseName}`);
-    await database.connect(configuration.databaseUrl, configuration.databaseName);
 });
