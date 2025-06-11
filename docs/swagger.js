f6478376f68eb5562bde3abb4cf11fe778f27019
CarlosL <carlos.ledesma1983@gmail.com>
2025-06-11 20:30:34 +0200

    feat: actualizar la interfaz de Swagger UI y mover el script a la carpeta de docs

---
+++ docs/swagger.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    SwaggerUIBundle({
+        url: '/swagger.json',
+        dom_id: '#swagger-ui',
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        layout: 'StandaloneLayout',
+    });
+});
--- static/swagger.js
+++
@@ -1,6 +0,0 @@
-SwaggerUIBundle({
-    url: 'http://localhost:3000/swagger.json',
-    dom_id: '#swagger-ui',
-    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-    layout: "StandaloneLayout"
-});
