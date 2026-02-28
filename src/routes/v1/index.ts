a56c7b6e78a04e2a990912efcdca60351d529bfa
Carlos L <carlos.developer1983@gmail.com>
2026-02-28 10:52:56 +0100

    feat: US-003 - export buildProjectRouter

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,3 +2,4 @@ export { default as buildUserRouter } from './user.js';
 export { default as buildGenerationRouter } from './generation.js';
 export { default as buildAuthRouter } from './auth.js';
 export { default as buildTeamRouter } from './team.js';
+export { default as buildProjectRouter } from './project.js';
